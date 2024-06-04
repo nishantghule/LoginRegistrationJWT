0ed7661316191e92e4daa0061b59578892119c2f
nishantghule <nishantghule128@gmail.com>
2024-06-04 19:37:58 +0530

    Project Upload

---
+++ src/main/java/com/example/loginregistartion/LoginRegistartionJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginregistartion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistartionJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistartionJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginregistartion/LoginRegistartionJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginregistartion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistartionJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
